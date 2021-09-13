afb0f61ece33c6cd180864e588d8b094d86fe410
Jason Viox <jrv04@hotmail.com>
2021-09-13 10:31:35 -0500

    Build service-worker cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,3 +8,16 @@ const FILES_TO_CACHE = [
 "/icon-192x192.png",
 "icon-512x512.png"
 ];
+
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/index.html"))
+    );
+
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    self.skipWaiting();
+});
+
