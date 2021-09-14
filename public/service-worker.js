a4023356c5a2a3ae62acca0e4f4eb55f80d98405
Jason Viox <jrv04@hotmail.com>
2021-09-14 10:13:38 -0500

    Edit / in index.js error

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,15 +5,12 @@ const FILES_TO_CACHE = [
 "/",
 "/index.html",
 "/styles.css",
-"index.js",
+"/index.js",
 "/icon-192x192.png",
 "icon-512x512.png"
 ];
 
 self.addEventListener("install", function (evt) {
-    evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/index.js"))
-    );
 
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
